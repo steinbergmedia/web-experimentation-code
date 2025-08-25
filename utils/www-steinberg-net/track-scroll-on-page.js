95ab817be909068b4cb76c263dd9a949e324b6fe
Marcel Birkhahn <m.birkhahn@steinberg.de>
2025-08-25 12:12:23 +0200

    reformat with prettier

--- utils/www-steinberg-net/track-scroll-on-page.js
+++ utils/www-steinberg-net/track-scroll-on-page.js
@@ -4,27 +4,27 @@ let hasFiredScroll75Event = false;
 let hasFiredScroll100Event = false;
 
 function handleScrollEvent() {
-    const windowHeight = window.innerHeight;
-    const bodyHeight = document.body.scrollHeight;
-    const scrollPercent = (window.scrollY / (bodyHeight - windowHeight)) * 100;
-    window.optimizely = window.optimizely || [];
+  const windowHeight = window.innerHeight;
+  const bodyHeight = document.body.scrollHeight;
+  const scrollPercent = (window.scrollY / (bodyHeight - windowHeight)) * 100;
+  window.optimizely = window.optimizely || [];
 
-    if (scrollPercent >= 25 && !hasFiredScroll25Event) {
-        window.optimizely.push({type: "event", eventName: "scroll25"});
-        hasFiredScroll25Event = true;
-    }
-    if (scrollPercent >= 50 && !hasFiredScroll50Event) {
-        window.optimizely.push({type: "event", eventName: "scroll50"});
-        hasFiredScroll50Event = true;
-    }
-    if (scrollPercent >= 75 && !hasFiredScroll75Event) {
-        window.optimizely.push({type: "event", eventName: "scroll75"});
-        hasFiredScroll75Event = true;
-    }
-    if (scrollPercent >= 100 && !hasFiredScroll100Event) {
-        window.optimizely.push({type: "event", eventName: "scroll100"});
-        hasFiredScroll100Event = true;
-    }
+  if (scrollPercent >= 25 && !hasFiredScroll25Event) {
+    window.optimizely.push({ type: "event", eventName: "scroll25" });
+    hasFiredScroll25Event = true;
+  }
+  if (scrollPercent >= 50 && !hasFiredScroll50Event) {
+    window.optimizely.push({ type: "event", eventName: "scroll50" });
+    hasFiredScroll50Event = true;
+  }
+  if (scrollPercent >= 75 && !hasFiredScroll75Event) {
+    window.optimizely.push({ type: "event", eventName: "scroll75" });
+    hasFiredScroll75Event = true;
+  }
+  if (scrollPercent >= 100 && !hasFiredScroll100Event) {
+    window.optimizely.push({ type: "event", eventName: "scroll100" });
+    hasFiredScroll100Event = true;
+  }
 }
 
-window.addEventListener('scroll', handleScrollEvent);
+window.addEventListener("scroll", handleScrollEvent);
